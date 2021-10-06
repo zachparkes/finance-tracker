77ac1948654e14543d29c9cbf9926f621c8d27bb
Zach Parkes <zach@sprigghr.com>
2021-10-06 12:01:10 -0400

    Complete project

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "my_portfolio", to: "users#my_portfolio"
   get "search_stock", to: "stocks#search"
+  get "my_friends", to: "users#my_friends"
+  get "search_friend", to: "users#search"
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
